6d18ae2bb141c71c53e7a4ade2d268f7919297ae
aliyazdani04 <98017374+aliyazdani04@users.noreply.github.com>
2022-02-09 10:57:23 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,221 @@
+from requests import get
+from re import findall
+import os
+import glob
+from rubika.client import Bot
+import requests
+from rubika.tools import Tools
+from rubika.encryption import encryption
+from gtts import gTTS
+from mutagen.mp3 import MP3
+import time
+import random
+import urllib
+import io
+
+print ("welcome, Created By Ali HL\n")
+
+print ("Please subscribe to the channel to receive updates! : for telegram : @ASY0405\n")
+
+bot = Bot(input("Please enter your Auth:"))
+target=input("Please Enter Your Guid (Group): ")
+
+print ("\nThe robot was successfully activated.")
+
+def hasAds(msg):
+	links = ["http://","https://",".ir",".com",".org",".net",".me"]
+	for i in links:
+		if i in msg:
+			return True
+			
+def hasInsult(msg):
+	swData = [False,None]
+	for i in open("dontReadMe.txt").read().split("\n"):
+		if i in msg:
+			swData = [True, i]
+			break
+		else: continue
+	return swData
+	
+# static variable
+answered, sleeped, retries = [], False, {}
+
+alerts, blacklist = [] , []
+
+def alert(guid,user,link=False):
+	alerts.append(guid)
+	coun = int(alerts.count(guid))
+
+	haslink = ""
+	if link : haslink = "گزاشتن لینک در گروه ممنوع میباشد .\n\n"
+
+	if coun == 1:
+		bot.sendMessage(target, "💢 اخطار [ @"+user+" ] \n"+haslink+" شما (1/3) اخطار دریافت کرده اید .\n\nپس از دریافت 3 اخطار از گروه حذف خواهید شد !\nجهت اطلاع از قوانین کلمه (قوانین) را ارسال کنید .")
+	elif coun == 2:
+		bot.sendMessage(target, "💢 اخطار [ @"+user+" ] \n"+haslink+" شما (2/3) اخطار دریافت کرده اید .\n\nپس از دریافت 3 اخطار از گروه حذف خواهید شد !\nجهت اطلاع از قوانین کلمه (قوانین) را ارسال کنید .")
+
+	elif coun == 3:
+		blacklist.append(guid)
+		bot.sendMessage(target, "🚫 کاربر [ @"+user+" ] \n (3/3) اخطار دریافت کرد ، بنابراین اکنون اخراج میشود .")
+		bot.banGroupMember(target, guid)
+
+
+while True:
+	# time.sleep(15)
+	try:
+		admins = [i["member_guid"] for i in bot.getGroupAdmins(target)["data"]["in_chat_members"]]
+		min_id = bot.getGroupInfo(target)["data"]["chat"]["last_message_id"]
+
+		while True:
+			try:
+				messages = bot.getMessages(target,min_id)
+				break
+			except:
+				continue
+
+		for msg in messages:
+			try:
+				if msg["type"]=="Text" and not msg.get("message_id") in answered:
+					if not sleeped:
+						if hasAds(msg.get("text")) and not msg.get("author_object_guid") in admins :
+							guid = msg.get("author_object_guid")
+							user = bot.getUserInfo(guid)["data"]["user"]["username"]
+							bot.deleteMessages(target, [msg.get("message_id")])
+							alert(guid,user,True)					
+								
+						elif msg.get("text").startswith("حذف") and msg.get("author_object_guid") in admins :
+							try:
+								number = int(msg.get("text").split(" ")[1])
+								answered.reverse()
+								bot.deleteMessages(target, answered[0:number])
+
+								bot.sendMessage(target, "✅ "+ str(number) +" پیام اخیر با موفقیت حذف شد", message_id=msg.get("message_id"))
+								answered.reverse()
+
+							except IndexError:
+								bot.deleteMessages(target, [msg.get("reply_to_message_id")])
+								bot.sendMessage(target, "✅ پیام با موفقیت حذف شد", message_id=msg.get("message_id"))
+							except:
+								bot.sendMessage(target, "❌ لطفا دستور را به درستی وارد کنید", message_id=msg.get("message_id"))
+
+						elif msg.get("text").startswith("اخراج") and msg.get("author_object_guid") in admins :
+							try:
+								guid = bot.getInfoByUsername(msg.get("text").split(" ")[1][1:])["data"]["chat"]["abs_object"]["object_guid"]
+								if not guid in admins :
+									bot.banGroupMember(target, guid)
+									# bot.sendMessage(target, "✅ کاربر با موفقیت از گروه اخراج شد", message_id=msg.get("message_id"))
+								else :
+									bot.sendMessage(target, "❌ کاربر ادمین میباشد", message_id=msg.get("message_id"))
+									
+							except IndexError:
+								bot.banGroupMember(target, bot.getMessagesInfo(target, [msg.get("reply_to_message_id")])[0]["author_object_guid"])
+								# bot.sendMessage(target, "✅ کاربر با موفقیت از گروه اخراج شد", message_id=msg.get("message_id"))
+							except:
+								bot.sendMessage(target, "❌ دستور اشتباه", message_id=msg.get("message_id"))						
+								
+						elif msg.get("text") == "پاک" and msg.get("author_object_guid") in admins :
+							try:
+								bot.deleteMessages(target, [msg.get("reply_to_message_id")])
+								bot.sendMessage(target, "پیام مورد نظر پاک شد...", message_id=msg.get("message_id"))
+							except:
+								print("err pak")	
+								
+						elif "forwarded_from" in msg.keys() and bot.getMessagesInfo(target, [msg.get("message_id")])[0]["forwarded_from"]["type_from"] == "Channel" and not msg.get("author_object_guid") in admins :
+							try:
+								print("Yek ahmagh forwared Zad")
+								bot.deleteMessages(target, [str(msg.get("message_id"))])
+								print("tabligh forearedi pak shod")
+							except:
+								print("err delete forwared")
+								
+						elif msg.get("text").startswith("اخطار") and msg.get("author_object_guid") in admins:
+							try:
+								user = msg.get("text").split(" ")[1][1:]
+								guid = bot.getInfoByUsername(user)["data"]["chat"]["abs_object"]["object_guid"]
+								if not guid in admins :
+									alert(guid,user)
+									
+								else :
+									bot.sendMessage(target, "❌ کاربر ادمین میباشد", message_id=msg.get("message_id"))
+									
+							except IndexError:
+								guid = bot.getMessagesInfo(target, [msg.get("reply_to_message_id")])[0]["author_object_guid"]
+								user = bot.getUserInfo(guid)["data"]["user"]["username"]
+								if not guid in admins:
+									alert(guid,user)
+								else:
+									bot.sendMessage(target, "❌ کاربر ادمین میباشد", message_id=msg.get("message_id"))
+							except:
+								bot.sendMessage(target, "❌ لطفا دستور را به درستی وارد کنید", message_id=msg.get("message_id"))
+
+
+
+						elif msg.get("text") == "قفل گروه" and msg.get("author_object_guid") in admins :
+							try:
+								bot.setMembersAccess(target, ["AddMember"])
+								bot.sendMessage(target, "🔒 گروه قفل شد", message_id=msg.get("message_id"))
+							except:
+								print("err lock GP")
+
+						elif msg.get("text") == "بازکردن گروه" or msg.get("text") == "باز کردن گروه" and msg.get("author_object_guid") in admins :
+							try:
+								bot.setMembersAccess(target, ["SendMessages","AddMember"])
+								bot.sendMessage(target, "🔓 گروه اکنون باز است", message_id=msg.get("message_id"))
+							except:
+								print("err unlock GP")
+								
+				elif msg["type"]=="Event" and not msg.get("message_id") in answered and not sleeped:
+					name = bot.getGroupInfo(target)["data"]["group"]["group_title"]
+					data = msg['event_data']
+					if data["type"]=="RemoveGroupMembers":
+						try:
+							user = bot.getUserInfo(data['peer_objects'][0]['object_guid'])["data"]["user"]["first_name"]
+							bot.sendMessage(target, f"‼️ کاربر {user} با موفقیت از گروه حذف شد .", message_id=msg["message_id"])
+							# bot.deleteMessages(target, [msg["message_id"]])
+						except:
+							print("err rm member answer")
+					
+					elif data["type"]=="AddedGroupMembers":
+					                user = bot.getUserInfo(data['peer_objects'][0]['object_guid'])["data"]["user"]["first_name"]
+					                bot.sendMessage(target, f"شلام {user} عزیز 😘🌹 \n • به گـروه {name} خیـلی خوش اومدی 😍❤️\n@RobotHL", message_id=msg["message_id"])
+					
+					elif data["type"]=="LeaveGroup":
+       						try:
+							            user = bot.getUserInfo(data['performer_object']['object_guid'])["data"]["user"]["first_name"]
+            							bot.sendMessage(target, f"اودافذ {user} 👋 ", message_id=msg["message_id"])
+            							# bot.deleteMessages(target, [msg["message_id"]])
+			      			except:
+		            					print("err Leave member Answer")		
+							
+					elif data["type"]=="JoinedGroupByLink":
+					                user = bot.getUserInfo(data['performer_object']['object_guid'])["data"]["user"]["first_name"]
+					                bot.sendMessage(target, f"شلام {user} عزیز 😘🌹 \n • به گـروه {name} خیـلی خوش اومدی 😍❤️\n@RobotHL", message_id=msg["message_id"])
+
+				else:
+					if "forwarded_from" in msg.keys() and bot.getMessagesInfo(target, [msg.get("message_id")])[0]["forwarded_from"]["type_from"] == "Channel" and not msg.get("author_object_guid") in admins :
+						bot.deleteMessages(target, [msg.get("message_id")])
+						guid = msg.get("author_object_guid")
+						user = bot.getUserInfo(guid)["data"]["user"]["username"]
+						bot.deleteMessages(target, [msg.get("message_id")])
+						alert(guid,user,True)
+					
+					continue
+			except:
+				continue
+
+			answered.append(msg.get("message_id"))
+			print("[" + msg.get("message_id")+ "] >>> " + msg.get("text") + "\n")
+
+	except KeyboardInterrupt:
+		exit()
+
+	except Exception as e:
+		if type(e) in list(retries.keys()):
+			if retries[type(e)] < 3:
+				retries[type(e)] += 1
+				continue
+			else:
+				retries.pop(type(e))
+		else:
+			retries[type(e)] = 1
+			continue
